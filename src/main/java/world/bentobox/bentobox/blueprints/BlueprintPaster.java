e9a8ea361c47b5c1feb252041c849ab392aa5d33
evlad <emmanuelvlad@gmail.com>
2022-05-23 18:31:28 +0200

    checking world's min height before pasting

--- src/main/java/world/bentobox/bentobox/blueprints/BlueprintPaster.java
+++ src/main/java/world/bentobox/bentobox/blueprints/BlueprintPaster.java
@@ -110,7 +110,9 @@ public class BlueprintPaster {
         // Offset due to bedrock
         Vector off = bp.getBedrock() != null ? bp.getBedrock() : new Vector(0,0,0);
         // Calculate location for pasting
-        this.location = island.getProtectionCenter().toVector().subtract(off).toLocation(world);
+        Location islandCenter = island.getProtectionCenter();
+        islandCenter.setY(Math.max(islandCenter.getY(), world.getMinHeight()));
+        this.location = islandCenter.toVector().subtract(off).toLocation(world);
     }
 
     private record Bits(Map<Vector, BlueprintBlock> blocks,
